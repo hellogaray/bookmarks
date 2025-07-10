4c382c140afc11df36c5e57497726273645a89e0
hellogaray <137473678+hellogaray@users.noreply.github.com>
2025-07-10 14:00:00 +0900

    Added CMS

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     // Copy some files and folders as-is to the output folder
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin');
     eleventyConfig.addPassthroughCopy("README.md");
     
     // Configure input and output directories
